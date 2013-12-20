e4c5725a2d4c788a9e5a085f5bd807cb979d46a4
mimipaskova <maria.paskova3@gmail.com>
2013-12-20 11:27:16 +0200

    Homework
    Counting Sort 9 task

---
+++ Arrays/09CountingSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09CountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masiv = new int[] { 4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3 };
+            int[] counting = new int[10];
+
+            //pulnim vtoriq masiv s 0
+            for (int i = 0; i < counting.Length; i++)
+            {
+                counting[i] = 0;
+            }
+
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                for (int j = 0; j < counting.Length; j++)
+                {
+                    if(masiv[i]==j)
+                    {
+                        counting[j]++;
+                    }
+                }
+            }
+
+            int max = counting[0];
+            int value = 0;
+            for (int i = 0; i < counting.Length; i++)
+            {
+                if (counting[i] > max)
+                {
+                    max = counting[i];
+                    value = i;
+                }
+            }
+            Console.WriteLine(value + " " + max + " times");
+
+        }
+    }
+}
