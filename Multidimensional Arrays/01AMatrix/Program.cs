b52cfdba49994249b947b23335e1ca716ebd1497
mimipaskova <maria.paskova3@gmail.com>
2013-12-23 21:46:09 +0200

    Homework
    Multidimensional arrays first task

---
+++ Multidimensional Arrays/01AMatrix/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01AMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int[,] matrix= new int[n, n];
+           // int[,]matrixTwo=new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = i+4*j+1;
+                }
+                
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j]+ " " );
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
