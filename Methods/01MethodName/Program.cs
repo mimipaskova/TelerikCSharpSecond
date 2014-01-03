9efca28161659a803195fd54afffb9590e80ec41
mimipaskova <maria.paskova3@gmail.com>
2014-01-03 22:34:56 +0200

    Homework
    Methods - first task (easy)

---
+++ Methods/01MethodName/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01MethodName
+{
+    class Program
+    {
+        static void PrintName(string Name)
+        {
+            Console.WriteLine("Hello" + " " + Name);
+        }
+
+        static void Main(string[] args)
+        {
+            string Name=Console.ReadLine();
+            PrintName(Name);
+        }
+    }
+}
