3226dc3b91fd52fc6771b71cdc0349c328cdada5
mimipaskova <maria.paskova3@gmail.com>
2014-01-04 12:35:00 +0200

    Homework Methods
    first three tasks from methods.

---
+++ Methods/02MaxNumberFrom3Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02MaxNumberFrom3Numbers
+{
+    class Program
+    {
+        static int GetMax(int numberOne, int numberTwo)
+        {
+            if (numberOne > numberTwo)
+                return numberOne;
+            else
+                return numberTwo;
+        }
+
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int numberThree = int.Parse(Console.ReadLine());
+            int max = GetMax(GetMax(numberOne, numberTwo), numberThree);
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ Methods/03LastDigit/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03LastDigit
+{
+    class Program
+    {
+        static int LastDigit(int number)
+        {
+            int last = number % 10;
+            return last;
+
+        }
+
+        static void LastDigitLetter(int number)
+        {
+            switch (LastDigit(number))
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                case 6:
+                    Console.WriteLine("Six");
+                    break;
+                case 7:
+                    Console.WriteLine("Seven");
+                    break;
+                case 8:
+                    Console.WriteLine("Eight");
+                    break;
+                case 9:
+                    Console.WriteLine("Nine");
+                    break;
+                case 0:
+                    Console.WriteLine("Zero");
+                    break;
+            }           
+        }
+
+        static void Main(string[] args)
+        {
+            int numnerOne = int.Parse(Console.ReadLine());
+            LastDigitLetter(numnerOne);
+        }
+    }
+}
