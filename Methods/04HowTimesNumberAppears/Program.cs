f788f010a1d81b3ba529b7110082f8e5dcb5e901
mimipaskova <maria.paskova3@gmail.com>
2014-01-05 17:03:38 +0200

    homework methods
    forth one - no true.

---
+++ Methods/04HowTimesNumberAppears/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04HowTimesNumberAppears
+{
+    class Program
+    {
+        static void NumberAppears(int[] masiv, int myNumber)
+        {
+            int[] number = new int[100];
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = 0;
+            }
+
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                for (int j = 0; j < masiv.Length; j++)
+                {
+
+
+                    if (masiv[i] == j)
+                    {
+                        number[i]++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (myNumber == i)
+                {
+                    Console.WriteLine(number[i+1]);
+                }
+                
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] masivOne=new int[]{1,2,4,4};
+            int chislo = int.Parse(Console.ReadLine());
+            NumberAppears(masivOne, chislo);
+
+        }
+    }
+}
