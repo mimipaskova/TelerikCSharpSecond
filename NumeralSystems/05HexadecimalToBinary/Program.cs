403638ac78b60842b4629c1acb632b204040dbd0
mimipaskova <maria.paskova3@gmail.com>
2014-01-12 23:15:18 +0200

    homework numeral systems 4/5/6 tasks done

---
+++ NumeralSystems/05HexadecimalToBinary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05HexadecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] length = new int[10];
+            string number = Console.ReadLine();
+            double[] resultOne = new double[10];
+            double result = 0;
+            int ostatuk = 0;
+            int convertSystem = 2;
+
+            int i = number.Length - 1;
+            while (i != -1)
+            {
+
+                // Console.WriteLine(number[i]);
+                switch (number[i])
+                {
+                    case 'A': ostatuk = 10; break;
+                    case 'B': ostatuk = 11; break;
+                    case 'C': ostatuk = 12; break;
+                    case 'D': ostatuk = 13; break;
+                    case 'E': ostatuk = 14; break;
+                    case 'F': ostatuk = 15; break;
+                    default: ostatuk = int.Parse(number[i].ToString());
+                        break;
+                }
+                // Console.WriteLine(ostatuk);
+
+
+                length[i] = number[i] % convertSystem;
+                number = number / convertSystem;
+                i++;
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ NumeralSystems/HexadecimalToDecimal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalToDecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            double[] resultOne = new double[10];
+            double result = 0;
+            int ostatuk = 0;
+
+            int i = number.Length-1;
+            while (i!=-1)
+            {
+
+               // Console.WriteLine(number[i]);
+                switch (number[i])
+                {
+                    case 'A': ostatuk = 10; break;
+                    case 'B': ostatuk = 11; break;
+                    case 'C': ostatuk = 12; break;
+                    case 'D': ostatuk = 13; break;
+                    case 'E': ostatuk = 14; break;
+                    case 'F': ostatuk = 15; break;
+                    default: ostatuk = int.Parse(number[i].ToString());
+                        break;
+                }
+               // Console.WriteLine(ostatuk);
+                
+                
+                result += ostatuk * Math.Pow(16, number.Length- i-1);
+                //number = number / 10;
+                i--;
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
